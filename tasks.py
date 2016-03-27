d68c37f9ec4dbb225e8a7c59aca6f9c1d964320b
Feihong Hsu <feihong.hsu@gmail.com>
2016-03-27 13:55:31 -0500

    Convert get_popular_songs script to task

--- tasks.py
+++ tasks.py
@@ -29,6 +29,38 @@ def print_favorite_songs():
     print_('\nListed %d songs' % count)
 
 
+@task
+def print_popular_songs():
+    """
+    Print songs rated 4 stars or above, grouped by artist.
+
+    """
+    class ArtistInfo:
+        def __init__(self):
+            self.genre = None
+            self.songs = []
+
+    # Map artists to ArtistInfo
+    artists = defaultdict(ArtistInfo)
+
+    tunes = itunes.ITunes()
+
+    good_tracks = (t for t in tunes.tracks if t.stars >= 4)
+    for t in good_tracks:
+        info = artists[t.artist]
+        info.genre = t.genre
+        info.songs.append(t.title)
+
+    artists = artists.items()
+    artists.sort(key=lambda x: len(x[1].songs), reverse=True)
+
+    for artist, info in artists:
+        print_(u'%s (%s):' % (artist, info.genre))
+        print_('- ' + u'\n- '.join(info.songs) + '\n')
+
+    print_('\nListed %d artists' % len(artists))
+
+
 def print_(s):
     """
     When printing to stdout, you need to encode to utf-8 or else you won't be

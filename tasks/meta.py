3f7a8975acef251ce49400421a4903daf4bae8f7
Feihong Hsu <feihong.hsu@gmail.com>
2017-11-23 13:39:34 -0600

    Add import_meta task

--- tasks/meta.py
+++ tasks/meta.py
@@ -18,10 +18,40 @@ def export(ctx):
                 title=track.title,
                 artist=track.artist,
                 rating=track.rating,
-                date_added=track.date_added,
             )
 
     output_path = Path('metadata.json')
     with output_path.open('w') as fp:
         for obj in gen():
             fp.write(json.dumps(obj) + '\n')
+
+
+@task
+def import_meta(ctx, input_file):
+    """
+    Import metadata from the given file
+
+    """
+    tmap = _create_track_map()
+    def gen():
+        input_path = Path(input_file)
+        with input_path.open() as fp:
+            for line in fp:
+                if line:
+                    yield json.loads(line)
+
+    for meta in gen():
+        key = (meta['title'], meta['artist'])
+        track = tmap.get(key)
+        _track = track._track
+        _track.setRating_(meta['rating'])
+
+
+
+def _create_track_map():
+    def gen():
+        for track in itunes.ITunes.tracks:
+            key = (track.title, track.artist)
+            yield key, track
+
+    return dict(gen())

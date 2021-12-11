f77382515eeefd8de7ad22a7afff1be7dbe9a1a8
Feihong Hsu <feihong.hsu@gmail.com>
2021-12-11 16:41:13 -0600

    Add script to delete all 2 star tracks

--- README.md
+++ README.md
@@ -15,3 +15,11 @@ Enter virtualenv
 List commands
 
     pipenv run inv -l
+
+Export all tracks to tracks.json
+
+    swift scripts/tracks_json.swift
+
+Generate new JSON files containing tracks rated 2 stars and below
+
+    jq 'map(select(.rating <= 2))' tracks.json > tracks-2-stars.json
---
+++ one-time/delete_2_star_tracks.py
@@ -0,0 +1,15 @@
+import json
+import os
+from pathlib import Path
+
+with open('tracks-2-stars.json') as fp:
+  tracks = json.load(fp)
+  confirm = input(f'Will delete {len(tracks)} files. Continue? ')
+
+  for track in tracks:
+    path = Path(track['location'])
+    if not path.exists():
+      print(f'{path} does not exist!')
+      break
+    print(path.exists(), path)
+    # os.remove(path)

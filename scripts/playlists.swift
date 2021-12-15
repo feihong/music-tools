a1f363ba43c1a0a7b94cf4ecc6b5a6af43362fe6
Feihong Hsu <feihong.hsu@gmail.com>
2021-12-15 15:13:39 -0600

    Improve playlists.swift

--- scripts/playlists.swift
+++ scripts/playlists.swift
@@ -1,4 +1,8 @@
-// https://developer.apple.com/documentation/ituneslibrary
+/*:
+https://developer.apple.com/documentation/ituneslibrary
+
+Print all music playlists. If a playlist name is provided, print all tracks in that playlist.
+*/
 
 import iTunesLibrary
 
@@ -8,7 +12,19 @@ guard let library = try? ITLibrary(apiVersion: "1.1") else {
 }
 
 let playlists = library.allPlaylists
+let args = CommandLine.arguments
 
-for playlist in playlists {
-  print("\(playlist.name) (\(playlist.items.count) items)")
+if let playlistName = args.count > 1 ? args[1].lowercased() : nil {
+  let matches = playlists.filter { $0.name.lowercased() == playlistName }
+  if matches.count == 0 {
+    print("No playlists with that name")
+  } else {
+    for track in matches[0].items {
+      print("\(track.title) by \(track.artist?.name ?? "")")
+    }
+  }
+} else {
+  for playlist in playlists {
+    print("\(playlist.name) (\(playlist.items.count) items)")
+  }
 }

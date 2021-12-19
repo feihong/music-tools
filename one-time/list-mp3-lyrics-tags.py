3a5068ec30e088a2809255055f22dc3cc08f2f47
Feihong Hsu <feihong.hsu@gmail.com>
2021-12-19 13:21:45 -0600

    Also list MP3 title tags

--- one-time/list-mp3-lyrics-tags.py
+++ one-time/list-mp3-lyrics-tags.py
@@ -9,13 +9,17 @@ def get_mp3s():
     except:
       print(f'{f} does not seem to be valid MP3 file')
 
+title_tags = set()
 lyrics_tags = set()
 
 for mp3 in get_mp3s():
   for key in mp3.tags.keys():
     if key.startswith('USLT'):
       lyrics_tags.add(key)
+    if key.startswith('TIT'):
+      title_tags.add(key)
 
 # Only one that should appear is USLT::eng
-for t in lyrics_tags:
-  print(t)
+print('Lyrics tags:', ', '.join(lyrics_tags))
+
+print('Title tags:', ', '.join(title_tags))
